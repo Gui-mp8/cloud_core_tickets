867106270cf4742d1b02f07c83d7b00517758861
Guilherme Machado <guilherme.pires@qualificarti.com.br>
2024-06-19 14:58:03 -0300

    .

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,14 +1,16 @@
 import os
-import pandas as pd
+# import pandas as pd
+# from google.cloud import bigquery
+from bigquery import BigQuery
 
-from google.cloud import bigquery
+# Set the environment variable for authentication
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = './billing-accounts-dashboard-credentials.json'
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = './billing-accounts-dashboard-credentials.json'
+# Load the CSV file into a DataFrame
+# df = pd.read_csv("gs://cloud_core/tickets-June-13-2024-15_22 - tickets-June-13-2024-15_22.csv.csv", sep=",", encoding="utf8")
 
-df = pd.read_csv("gs://cloud_core/tickets-June-13-2024-15_22 - tickets-June-13-2024-15_22.csv.csv", sep=",", encoding="utf8")
-
-client = bigquery.Client()
-
-
-
-print(df)
+BigQuery().truncate_csv_file(
+    storage_file_path="gs://cloud_core/tickets-June-13-2024-15_22 - tickets-June-13-2024-15_22.csv.csv",
+    dataset_name="cloud_core",
+    table_name="tickets"
+)
